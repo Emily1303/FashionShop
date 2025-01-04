063b2b9af1bf3af85c0f9acc63fac3b91a866210
Emily1303 <e.filipova1303@gmail.com>
2025-01-04 23:11:16 +0200

    Added files for working with Bootstrap, templates, controllers and navbar fragment

---
+++ src/main/java/com/example/fashionshop/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.fashionshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String indexPage() {
+
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+
+        return "home";
+    }
+}
